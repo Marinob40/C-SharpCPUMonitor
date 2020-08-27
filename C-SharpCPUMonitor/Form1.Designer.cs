96e8e0442accd78c3a3248da5b5936a9c8df2c3e
Marino Blocker <marinoblocker31@gmail.com>
2020-08-27 07:19:04 -0400

    First Commit

---
+++ C-SharpCPUMonitor/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace C_SharpCPUMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblCPU = new System.Windows.Forms.Label();
+            this.lblMemory = new System.Windows.Forms.Label();
+            this.lblSystem = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblCPU
+            // 
+            this.lblCPU.AutoSize = true;
+            this.lblCPU.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPU.ForeColor = System.Drawing.Color.Gainsboro;
+            this.lblCPU.Location = new System.Drawing.Point(120, 61);
+            this.lblCPU.Name = "lblCPU";
+            this.lblCPU.Size = new System.Drawing.Size(0, 20);
+            this.lblCPU.TabIndex = 0;
+            // 
+            // lblMemory
+            // 
+            this.lblMemory.AutoSize = true;
+            this.lblMemory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMemory.ForeColor = System.Drawing.Color.Gainsboro;
+            this.lblMemory.Location = new System.Drawing.Point(120, 129);
+            this.lblMemory.Name = "lblMemory";
+            this.lblMemory.Size = new System.Drawing.Size(0, 20);
+            this.lblMemory.TabIndex = 1;
+            // 
+            // lblSystem
+            // 
+            this.lblSystem.AutoSize = true;
+            this.lblSystem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSystem.ForeColor = System.Drawing.Color.Gainsboro;
+            this.lblSystem.Location = new System.Drawing.Point(120, 205);
+            this.lblSystem.Name = "lblSystem";
+            this.lblSystem.Size = new System.Drawing.Size(51, 20);
+            this.lblSystem.TabIndex = 2;
+            this.lblSystem.Text = "label3";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(389, 368);
+            this.Controls.Add(this.lblSystem);
+            this.Controls.Add(this.lblMemory);
+            this.Controls.Add(this.lblCPU);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Monitor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCPU;
+        private System.Windows.Forms.Label lblMemory;
+        private System.Windows.Forms.Label lblSystem;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ C-SharpCPUMonitor/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace C_SharpCPUMonitor
+{
+    public partial class Form1 : Form
+    {
+
+        PerformanceCounter perfCPUCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+        PerformanceCounter perfMemoryCounter = new PerformanceCounter("Memory", "Available MBytes");
+        PerformanceCounter perfSystemCounter = new PerformanceCounter("System", "System Up Time");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblCPU.Text = "CPU:" + "  " + (int)perfCPUCounter.NextValue() + "  " + "%";
+            lblMemory.Text = "Available Memory:" + "  " + (int)perfMemoryCounter.NextValue() + "  " + "MB";
+            lblSystem.Text = "System Up Time:" + "  " + (int)perfSystemCounter.NextValue() / 60 /60 + "Hours";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
